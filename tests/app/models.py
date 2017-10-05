6d89f968f8de71f4e0f9aea8cd699e6d9a17da8a
Taylor C. Richberger <Taywee@gmx.com>
2017-10-05 16:53:50 -0600

    build out tests directory

---
+++ tests/app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+from wagtail.wagtailcore.models import Page
+
+class TestPage(Page):
+    pass
---
+++ tests/app/settings.py
@@ -0,0 +1,90 @@
+import os
+import sys
+
+
+def env(name, default=None):
+    if sys.version_info < (3,):
+        return os.environ.get(name, failobj=default)
+    else:
+        return os.environ.get(name, default=default)
+
+
+INSTALLED_APPS = [
+    'wagtailapproval',
+    'tests.app',
+
+    'wagtail.wagtailforms',
+    'wagtail.wagtailredirects',
+    'wagtail.wagtailembeds',
+    'wagtail.wagtailsites',
+    'wagtail.wagtailusers',
+    'wagtail.wagtailsnippets',
+    'wagtail.wagtaildocs',
+    'wagtail.wagtailimages',
+    'wagtail.wagtailsearch',
+    'wagtail.wagtailadmin',
+    'wagtail.wagtailcore',
+
+    'modelcluster',
+    'taggit',
+
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+ALLOWED_HOSTS = ['localhost']
+
+SECRET_KEY = 'not a secret'
+
+ROOT_URLCONF = 'tests.app.urls'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': env('DATABASE_NAME', 'test.sqlite3'),
+    },
+}
+
+WAGTAIL_SITE_NAME = 'Wagtail Approval'
+
+DEBUG = True
+
+USE_TZ = True
+TIME_ZONE = 'America/Denver'
+
+MIDDLEWARE_CLASSES = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'wagtail.wagtailcore.middleware.SiteMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.template.context_processors.request',
+                'django.contrib.messages.context_processors.messages'
+            ],
+        },
+    },
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+STATIC_URL = '/static/'
